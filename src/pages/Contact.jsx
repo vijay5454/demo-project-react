c7c5022965e61da652a8d2b3938400df74357285
vijay5454 <vijaythalaiva5454@gmail.com>
2023-04-04 18:02:47 +0530

    tried radio button :)

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 const Contact = () => {
+  // const [selectedAccount, setSelectedAccount] = useState("");
+
+  // const handleRadio = (e) => {
+  //   setSelectedAccount(e.target.value);
+  // };
   return (
     <section className="contact">
       <div className="section-center">
@@ -10,6 +17,38 @@ const Contact = () => {
           Name: Vijay K <br /> Phone Number: 9003324408 <br /> Email:
           vijayk90033@gmail.com
         </p>
+        {/* <div className="account-details-list">
+          <div className="account-details">
+            <h4>1235525</h4>
+            <h4>Every-day Savings</h4>
+            <input
+              type="radio"
+              name="account-list"
+              value="Every-day Savings"
+              onClick={handleRadio}
+            />
+          </div>
+          <div className="account-details">
+            <h4>1235525</h4>
+            <h4>Flexi Saver</h4>
+            <input
+              type="radio"
+              name="account-list"
+              value="Flexi Saver"
+              onClick={handleRadio}
+            />
+          </div>
+          <div className="account-details">
+            <h4>1235525</h4>
+            <h4>Club LLoyds Current</h4>
+            <input
+              type="radio"
+              name="account-list"
+              value="Club LLoyds Current"
+              onClick={handleRadio}
+            />
+          </div>
+        </div> */}
       </div>
     </section>
   );
